9dc8198741f68c0d0855a5fe183b4a433c0d4ec3
fedorka3 <35789@ocvts.org>
2025-01-10 13:55:51 +0000

    arrow system

--- script.js
+++ script.js
@@ -1,14 +1,46 @@
+// Select all images in the gallery
 const galleryImages = document.querySelectorAll('#gallery-container img');
+const images = Array.from(galleryImages); // convert modeList to array
+// Select elements for lightbox
 const lightbox = document.getElementById('lightbox')
 const lightboxImg = document.getElementById('lightbox-img');
 
-galleryImages.forEach(image => {
+let currentIndex = 0;
+
+galleryImages.forEach((image, index) => {
     image.addEventListener('click', () => {
+        currentIndex = index;
         lightboxImg.src = image.src;
         lightbox.classList.add('visible');
     })
 })
 
-lightbox.addEventListener('click', () => {
-    lightbox.classList.remove('visible');
+lightbox.addEventListener('click', (event) => {
+    if(event.target === lightbox) {
+        lightbox.classList.remove('visible');
+    }
+})
+
+document.getElementById('next').addEventListener('click', () => {
+    currentIndex = (currentIndex + 1) % images.length;
+    lightboxImg.src = images[currentIndex].src;
+
+})
+
+document.getElementById('prev').addEventListener('click', () => {
+    currentIndex = (currentIndex - 1 + images.length) % images.length;
+    lightboxImg.src = images[currentIndex].src;
+
+})
+
+document.addEventListener('keydown', (event) => {
+    if (event.key === 'ArrowRight') {
+        currentIndex = (currentIndex + 1) % images.length;
+        lightboxImg.src = images[currentIndex].src;
+    } else if (event.key === 'ArrowLeft') {
+        currentIndex = (currentIndex - 1 + images.length) % images.length;
+        lightboxImg.src = images[currentIndex].src;
+    } else if (event.key === 'Escape') {
+        lightbox.classList.remove('visible');
+    }
 })
